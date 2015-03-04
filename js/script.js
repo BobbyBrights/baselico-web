4353506dbd8729cea469e58d84988fe12323c493
Wagner De Paula <wagner.de.paula@teamaol.com>
2015-03-04 10:33:31 -0500

    …

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    'use strict';
+    
+    
+    var watchPhoto = $('.photo img');
+   	var watchPhotoWidth = watchPhoto.width();
+   	var watchPhotoHeight = watchPhoto.height();
+   	
+   	
+    
+    
+});
